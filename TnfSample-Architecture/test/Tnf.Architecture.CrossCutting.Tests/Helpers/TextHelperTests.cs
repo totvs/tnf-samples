8e96d64ed9454a4948488c166f4c258ff3548fc1
Alexandre Morales <alexandre.fraga@totvs.com.br>
2017-04-28 15:23:53 -0300

    Alterações linux

---
+++ TnfSample-Architecture/test/Tnf.Architecture.CrossCutting.Tests/Helpers/TextHelperTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tnf.Architecture.Dto.Tests.Helpers
+{
+    public class TextHelperTests
+    {
+        [Fact]
+        public void Should_Resolve_Controller()
+        {
+            ServiceProvider.GetService<CountryController>().ShouldNotBeNull();
+        }
+    }
+}
