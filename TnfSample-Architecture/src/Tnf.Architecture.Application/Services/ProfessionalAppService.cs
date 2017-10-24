215fb5f3bcc74c934cf9d614ff34390076f8dbfc
Alexandre Morales <alexandre.fraga@totvs.com.br>
2017-10-24 19:28:12 -0200

    Revert "Removido Rabbit"
    This reverts commit 052282aa95ff810bdb9c74e8e1cc17304d422ad4.

--- TnfSample-Architecture/src/Tnf.Architecture.Application/Services/ProfessionalAppService.cs
+++ TnfSample-Architecture/src/Tnf.Architecture.Application/Services/ProfessionalAppService.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Tnf.App.Application.Services;
 using Tnf.App.AutoMapper;
+using Tnf.App.Bus.Client;
+using Tnf.App.Bus.Queue.Interfaces;
 using Tnf.App.Dto.Request;
 using Tnf.App.Dto.Response;
+using Tnf.Architecture.Application.Commands;
+using Tnf.Architecture.Application.Events;
 using Tnf.Architecture.Application.Interfaces;
 using Tnf.Architecture.Common;
 using Tnf.Architecture.Common.Enumerables;
@@ -15,8 +20,16 @@ using Tnf.Architecture.EntityFrameworkCore.ReadInterfaces;
 
 namespace Tnf.Architecture.Application.Services
 {
-    public class ProfessionalAppService : AppApplicationService, IProfessionalAppService
+    public class ProfessionalAppService : AppApplicationService, IProfessionalAppService,
+        IPublish<SpecialtyCreateCommand>, // Informa que essa classe publicará uma mensagem (SpecialtyCreateCommand)
+        ISubscribe<SpecialtyCreatedEvent> // Informa que essa classe assinará uma mensagem (SpecialtyCreatedEvent)
     {
+        /// <summary>
+        /// Propriedade de controle - utilizada para observar
+        /// o processamento da fila no Sample
+        /// </summary>
+        public static int SpecialtyCreatedEventIdHelper { get; set; }
+
         private readonly IProfessionalService _service;
         private readonly IProfessionalReadRepository _readRepository;
 
@@ -69,8 +82,59 @@ namespace Tnf.Architecture.Application.Services
                 .WithName(professional.Name)
                 .WithPhone(professional.Phone)
                 .WithEmail(professional.Email)
-                .WithAddress(professional.Address)
-                .WithSpecialties(
+                .WithAddress(professional.Address);
+
+            /* -----------------------------
+             * Lógica para sample com Fila
+             * -----------------------------
+             * #Sobre
+             * Para fins didáticos, o sample foi alterado para permitir a demostração
+             * do conceito dentro do mesmo contexto de WebApi em uma única aplicação.
+             * O controle é feito através da observação da propriedade estática this.SpecialtyCreatedEventIdHelper
+             * 
+             * #Fluxo             
+             * -> Swagger - Cria payload com dados do Professional e uma única Specialty com descrição e Id = 0
+             *    ! Sample no final da classe
+             * -> Swagger - Faz Post de um novo Professional para a API de Professional
+             * -> ProfessionalAppService - (Id = 0) Publica uma mensagem de Command para criação de uma nova Specialty
+             * -> SpecialtyAppService - Assina o command, logo, irá processar o command.
+             * -> SpecialtyAppService - Finaliza criação da nova Specialty e publica evento de nova Specialty criada
+             * -> ProfessionalAppService - Assina evento de nova Specialty
+             * -> ProfessionalAppService - Atualiza propriedade estática com o Id da nova Specialty
+             * -> ProfessionalAppService - Libera looping while e finaliza processo de criação de novo Professional
+             */
+
+            if (professional.Specialties.Count > 0 &&
+                professional.Specialties.First().Id == 0)
+            {
+                // Cria Command para solicitar criação de Specialty
+                var specialtyCreateCommand = new SpecialtyCreateCommand
+                {
+                    Description = professional.Specialties.First().Description
+                };
+
+                // Invoca publicação do Command
+                Handle(specialtyCreateCommand);
+
+                // Controle - Agaurda processamento do Command
+                while (SpecialtyCreatedEventIdHelper == 0)
+                    // Quando Command é finalizado, evento gerado atualiza valor de SpecialtyCreatedEventIdHelper
+                    Task.Delay(-1);
+
+                // Adiciona o Id da nova Specialty no Builder de Professional
+                professionalBuilder.WithSpecialties(
+                    professional
+                    .Specialties
+                    .Select(s =>
+                        new SpecialtyBuilder(Notification)
+                            .WithId(SpecialtyCreatedEventIdHelper)
+                            .WithDescription(s.Description)
+                            .Build())
+                            .ToList());
+            }
+            else
+            {
+                professionalBuilder.WithSpecialties(
                     professional
                     .Specialties
                     .Select(s =>
@@ -79,6 +143,7 @@ namespace Tnf.Architecture.Application.Services
                             .WithDescription(s.Description)
                             .Build())
                             .ToList());
+            }
 
             var id = _service.CreateProfessional(professionalBuilder);
 
@@ -156,5 +221,44 @@ namespace Tnf.Architecture.Application.Services
                                                 .WithMessageFormat(parameter)
                                                 .Build());
         }
+
+        /// <summary>
+        /// Processa mensagem para Publicação
+        /// </summary>
+        /// <param name="message">Mensagem que será publicada</param>
+        public void Handle(SpecialtyCreateCommand message) =>
+            // Método de extensão para publicação da mensagem
+            message.Publish();
+
+        /// <summary>
+        /// Processa uma mensagem assinada
+        /// </summary>
+        /// <param name="message">Mensagem recebida por assinatura</param>
+        public void Handle(SpecialtyCreatedEvent message) =>
+            // Recebe mensagem da assinatura do evento e atualiza propriedade de controle SpecialtyCreatedEventIdHelper
+            SpecialtyCreatedEventIdHelper = message.SpecialtyId;
     }
 }
+
+/*
+ * Payload Sample de Novo Professional
+ {
+  "name": "Steve Jobs",
+  "address": {
+    "street": "Rua Do Comercio",
+    "number": "123",
+    "complement": "APT 123",
+    "zipCode": {
+      "number": "12345678"
+    }
+  },
+  "phone": "98653214",
+  "email": "steve@apple.com",
+  "specialties": [
+    {
+      "description": "Genius",
+      "id": 0
+    }
+  ]
+}
+ */
--- TnfSample-Architecture/src/Tnf.Architecture.Web/Startup/WebModule.cs
+++ TnfSample-Architecture/src/Tnf.Architecture.Web/Startup/WebModule.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Tnf.App.AspNetCore;
+using Tnf.App.Bus.Client;
+using Tnf.App.Bus.Client.Configuration.Startup;
+using Tnf.App.Bus.Queue;
+using Tnf.App.Bus.Queue.Enums;
+using Tnf.App.Bus.Queue.RabbitMQ;
 using Tnf.App.Configuration;
 using Tnf.App.Security.Identity;
 using Tnf.Architecture.Application;
+using Tnf.Architecture.Application.Commands;
+using Tnf.Architecture.Application.Events;
 using Tnf.Architecture.Common;
 using Tnf.Modules;
 
@@ -12,6 +19,8 @@ namespace Tnf.Architecture.Web.Startup
     [DependsOn(
         typeof(AppModule),
         typeof(TnfAppSecurityIdentityModule),
+        typeof(TnfAppBusQueueModule), // <- Módulo para Fila
+        typeof(TnfAppBusClientModule), // <- Módulo para manipulação da Fila
         typeof(TnfAppAspNetCoreModule))]
     public class WebModule : TnfModule
     {
@@ -28,6 +37,58 @@ namespace Tnf.Architecture.Web.Startup
                                     .FromJsonFiles(_env.ContentRootPath, $"appsettings.{_env.EnvironmentName}.json");
 
             Configuration.DefaultNameOrConnectionString = configuration.GetConnectionString(AppConsts.ConnectionStringName);
+            
+            #region Setup Mensageria via Builder - Veja documentação TDN
+
+            // Cria um Tópico da mensagem SpecialtyCreateCommand
+            var specialtyCreateCommandTopic = TopicSetup.Builder
+                .Factory()
+                .Message<SpecialtyCreateCommand>()
+                .AddKey("Specialty.Create.Command")
+                .Build();
+
+            // Cria um Tópico da mensagem SpecialtyCreatedEvent
+            var specialtyCreatedEventTopic = TopicSetup.Builder
+                .Factory()
+                .Message<SpecialtyCreatedEvent>()
+                .AddKey("Specialty.Created.Event")
+                .Build();
+
+            // Cria uma Fila
+            var queue = QueueSetup.Builder
+               .Factory()
+               .QueueName("General")
+               .QueueReliabilitySetup(r => r
+                   .AckIsMandatory(true)
+                   .AutoDeleteQueue(true)
+                   .MaxMessageSize(256)
+                   .PersistMessage(false)
+                   .Build())
+               .AddTopics(specialtyCreateCommandTopic)
+               .AddTopics(specialtyCreatedEventTopic)
+               .Build();
+
+            // Cria um Exchange Router
+            var exchangeRouter = ExchangeRouter
+                .Builder
+                .Factory()
+                .Name("ExchangeForTesting")
+                .ServerAddress("127.0.0.1")
+                .Type(ExchangeType.topic)
+                .QueueChannel(QueueChannel.Amqp)
+                .Reliability(isDurable: false, isAutoDelete: false, isPersistent: false)
+                .AddQueue(queue)
+                .SetExclusive(false)
+                .Build();
+
+            #endregion
+
+            // Configura Módulo Mensageria
+            Configuration.BusClientSetup()
+                .SetExchangeRouter("default", e => exchangeRouter)
+                .AddPublishers(() => new PublisherListener(Configuration.BusClientSetup().GetExchangeRouterInstance("default")))
+                .AddSubscribers(() => new SubscriberListener(Configuration.BusClientSetup().GetExchangeRouterInstance("default")))
+                .Run();
         }
 
         public override void Initialize()
