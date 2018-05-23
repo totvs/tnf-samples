2e2fdcbb732d358878ca9e646718cb0506a50cc1
Josué Dall'Agnese <josue.agnese@totvs.com.br>
2018-05-23 11:32:24 -0300

    Atualização do Rac para um deploy unificado

--- Scenarios/Security/Rac/_StartRacInfrastructure.cmd
+++ Scenarios/Security/Rac/_StartRacInfrastructure.cmd
@@ -1,10 +1,3 @@
-cd .\Host
-start "Tnf.Rac.Host.dll" dotnet Tnf.Rac.Host.dll
+cd .\Rac
+start "Tnf.Rac.Host.All.dll" dotnet Tnf.Rac.Host.All.dll
 cd ..
-
-cd .\Authorization
-start "Tnf.Rac.Host.Authorization.dll" dotnet Tnf.Rac.Host.Authorization.dll
-cd ..
-
-cd .\Management
-start "Tnf.Rac.Management.Api.dll" dotnet Tnf.Rac.Management.Api.dll
