e4a5f84fc7bf1a2bea61deee2cdd1da8168047f0
Josué Dall'Agnese <josofd@gmail.com>
2019-08-07 12:59:31 -0300

    Atualização de versão do TNF para 2.7.0.21802

--- Scenarios/Messaging/src/Messaging.Web1/Startup.cs
+++ Scenarios/Messaging/src/Messaging.Web1/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Tnf.Bus.Queue.RabbitMQ;
+using Tnf.Configuration;
 using Tnf.Localization;
 
 namespace Messaging.Web1
--- Scenarios/Messaging/src/Messaging.Web2/Startup.cs
+++ Scenarios/Messaging/src/Messaging.Web2/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Tnf.Bus.Queue.RabbitMQ;
+using Tnf.Configuration;
 
 namespace Messaging.Web2
 {
