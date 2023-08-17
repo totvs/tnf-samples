f27ea5527fe6620608de816b6efd668253b974c3
peterson.cardoso@totvs.com.br <Piterod@tico95>
2023-08-17 08:59:47 -0300

    feat: added unified command

---
+++ Scenarios/CarShopCrud/src/Tnf.CarShop.Application/Commands/Car/CarCommand/CarCommand.cs
@@ -0,0 +1,30 @@
+﻿using Tnf.CarShop.Application.Dtos;
+
+namespace Tnf.CarShop.Application.Commands.Car.Create;
+
+public sealed record CarCommand
+{
+    public Guid? Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class CarResult
+{
+    public CarResult(Guid createdCarId, bool success)
+    {
+        CarId = createdCarId;
+        Success = success;
+    }
+
+    public CarResult(CarDto carDto)
+    {
+        CarDto = carDto;
+    }
+
+    public CarDto CarDto { get; set; }
+    public Guid CarId { get; set; }
+    public bool Success { get; set; }
+}
---
+++ Scenarios/CarShopCrud/src/Tnf.CarShop.Application/Commands/Car/CarCommand/CarCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using Tnf.CarShop.Domain.Repositories;
+using Tnf.Commands;
+using Tnf.CarShop.Application.Dtos;
+using System;
+using Tnf.CarShop.Application.Commands.Car.Create;
+using Tnf.CarShop.Application.Commands.Car.Update;
+
+namespace Tnf.CarShop.Application.Commands.Car;
+
+public class CarCommandHandler : CommandHandler<CarCommand, CarResult>
+{
+    private readonly ICarRepository _carRepository;
+    private readonly ILogger<CarCommandHandler> _logger;
+
+    public CarCommandHandler(
+        ILogger<CarCommandHandler> logger,
+        ICarRepository carRepository)
+    {
+        _logger = logger;
+        _carRepository = carRepository;
+    }
+
+    public override async Task<CarResult> ExecuteAsync(CarCommand command, CancellationToken cancellationToken = default)
+    {
+        if (command.Id.HasValue)
+        {
+            var car = await _carRepository.GetAsync(command.Id.Value, cancellationToken);
+            if (car == null) throw new Exception($"Car with id {command.Id.Value} not found.");
+
+            car.UpdateBrand(command.Brand);
+            car.UpdateModel(command.Model);
+            car.UpdatePrice(command.Price);
+            car.UpdateYear(command.Year);
+
+            var updatedCar = await _carRepository.UpdateAsync(car, cancellationToken);
+            var updatedCarDto = new CarDto(updatedCar.Id, updatedCar.Brand, updatedCar.Model, updatedCar.Year, updatedCar.Price);
+
+            return new CarResult(updatedCarDto);
+        }
+        else
+        {
+            var newCar = new Domain.Entities.Car(command.Brand, command.Model, command.Year, command.Price);
+            var createdCar = await _carRepository.InsertAsync(newCar, cancellationToken);
+
+            return new CarResult(createdCar.Id, true);
+        }
+       
+    }
+}
