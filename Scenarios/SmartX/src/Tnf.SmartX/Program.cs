78655e2839684f67a2341fd9db8bdbcbfbaec8df
Raphael de Moraes <raphaeld.moraes@totvs.com.br>
2025-07-04 15:09:36 -0300

    add api with swagger

---
+++ Scenarios/SmartX/src/Tnf.SmartX/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tnf.SmartX;
+using Tnf.SmartX.EntityFramework.PostgreSql;
+using Tnf.SmartX.Sample.Swagger;
+
+const string CodeFirstConnectionStringName = "CodeFirst";
+const string DatabaseFirstConnectionStringName = "DatabaseFirst";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTnfAspNetCore(b =>
+{
+    b.ApplicationName("TNF SmartX Sample");
+    b.DefaultConnectionString(builder.Configuration.GetConnectionString(CodeFirstConnectionStringName));
+    b.MultiTenancy(c => c.IsEnabled = true);
+});
+
+builder.Services.AddFluigIdentitySecurity(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddCorsAll("AllowAll");
+
+builder.Services.AddSmartXApiVersioning();
+
+builder.Services.AddEFCorePostgreSql(builder.Configuration);
+
+builder.Services.AddTnfSmartX(b =>
+{
+    b.ConfigureEfCodeFirst();
+    b.ConfigureEfDatabaseFirst(configure => configure.UseNpgsql(builder.Configuration.GetConnectionString(DatabaseFirstConnectionStringName)));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
